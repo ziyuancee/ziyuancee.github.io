b4e0c16d3c8df75e33f93c538ed5548b1dcc3952
Ching Ziyuan <zching1@sheffield.ac.uk>
2023-04-26 19:50:53 +0100

    Added option to update tokens

--- js/settings.js
+++ js/settings.js
@@ -11,6 +11,7 @@ var fbApp = document.getElementById('fbapp');
 var fbMeasurement = document.getElementById('fbmeasurement');
 var fbToken = document.getElementById('fbtoken');
 var optionSubmit = document.getElementById('option_submit');
+var optionUpdate = document.getElementById('option_update');
 
 if (adaUserCookie !== undefined && adaApiCookie !== undefined) {
     coDangerLatest(false);
@@ -27,6 +28,24 @@ optionSubmit.addEventListener("click", function() {
         }
     }
 });
+if (fbTokenCookie !== undefined && adaUserCookie !== undefined && adaApiCookie !== undefined) {
+    optionUpdate.addEventListener("click", function() {
+        const form = new FormData();
+        form.append('value', fbTokenCookie);
+        fetch('https://io.adafruit.com/api/v2/' + adaUserCookie + '/feeds/tokens/data', {
+                method: 'POST',
+                headers: {
+                    'X-AIO-Key': adaApiCookie
+                },
+                body: form
+            })
+            .then(response => response.json())
+            .then(response => console.log(JSON.stringify(response)))
+    });
+} else {
+    optionUpdate.remove();
+}
+
 for (var i = 0; i < valueArray.length; i++) {
     if (Cookies.get(valueArray[i]) !== undefined) {
         document.getElementById(valueArray[i]).value = Cookies.get(valueArray[i]);
